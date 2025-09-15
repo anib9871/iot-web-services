0080e676c2174acb5cdfba955d70c18099f3a964
anib9871 <anib9871@gmail.com>
2025-09-15 13:46:07 +0530

    First commit for Railway

---
+++ iot_project/settings.py
@@ -0,0 +1,96 @@
+# iot_project/settings.py
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-your-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'iot_api',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'iot_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'iot_api' / 'templates'],  # ← Path object
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'iot_project.wsgi.application'
+
+# SQLite
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
+DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'airkpi',  # Replace with your database name
+                'USER': 'root',      # Replace with your MySQL username
+                'PASSWORD': '1234', # Replace with your MySQL password
+                'HOST': 'localhost',             # Or the IP address/hostname of your MySQL server
+                'PORT': '3306',                  # Default MySQL port
+            }
+        }
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny"
+    ]
+}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Kolkata'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "testwebservice71@gmail.com" 
+EMAIL_HOST_PASSWORD = "akuu vulg ejlg ysbt"  
+
+TWILIO_ACCOUNT_SID = '721caf7fd9f3071ACc3786c5763f4cd1d8'
+TWILIO_AUTH_TOKEN = 'a364782f1c2ea9b02b1e30915505043d'
+TWILIO_PHONE_NUMBER = '+17755877724'  # Twilio se mila number
+
